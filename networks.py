6d0bb51f57b2321d484cc050d0b5f0b180982b12
sharad1126 <sharad.agarwal2014@vit.ac.in>
2018-03-03 17:23:37 +0100

    Add generator class

--- networks.py
+++ networks.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+
 class Discriminator(torch.nn.Module):
     """
     A three hidden-layer discriminative neural network
@@ -36,6 +37,42 @@ class Discriminator(torch.nn.Module):
         x = self.out(x)
         return x
 
+class Generator(torch.nn.Module):
+    """
+    A three hidden-layer generative neural network
+    """
+    def __init__(self):
+        super(Generator, self).__init__()
+        n_features = 100
+        n_out = 784
+
+        self.hidden0 = nn.Sequential(
+            nn.Linear(n_features, 256),
+            nn.LeakyReLU(0.2)
+        )
+        self.hidden1 = nn.Sequential(
+            nn.Linear(256, 512),
+            nn.LeakyReLU(0.2)
+        )
+        self.hidden2 = nn.Sequential(
+            nn.Linear(512, 1024),
+            nn.LeakyReLU(0.2)
+        )
+
+        self.out = nn.Sequential(
+            nn.Linear(1024, n_out),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = self.hidden0(x)
+        x = self.hidden1(x)
+        x = self.hidden2(x)
+        x = self.out(x)
+        return x
+
 if __name__ == "__main__":
     net = Discriminator()
-    print(net)
+    print("Discriminator ", net)
+    net = Generator()
+    print("Generator ", net)
