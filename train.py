43964443f837bae98c6bd332a690faeb1719df4f
sharad1126 <sharad.agarwal2014@vit.ac.in>
2018-03-03 17:33:42 +0100

    Function to train discriminator

--- train.py
+++ train.py
@@ -3,7 +3,9 @@
 import torch
 from torchvision import transforms, datasets
 from networks import Discriminator, Generator
-from utils import noise
+from utils import noise, images_to_vectors, vectors_to_images
+from utils import real_data_target, fake_data_target
+
 
 DATA_FOLDER = './torch_data/VGAN/MNIST'
 use_gpu = torch.cuda.is_available()
@@ -45,3 +47,26 @@ Define loss
 """
 loss = torch.nn.BCELoss()
 NUM_EPOCHS = 200
+
+
+"""
+function to train discriminator
+"""
+def train_discriminator(optimizer, real_data, fake_data):
+    # reset gradients
+    optimizer.zero_grad()
+
+    # Train on real data
+    prediction_real = discriminator(real_data)
+    # Calculate error and back propagate
+    error_real = loss(prediction_real, real_data_target(real_data.size(0)))
+    error_real.backward()
+
+    # Train on fake data
+    prediction_fake = discriminator(fake_data)
+    # Calculate error and back propagate
+    error_fake = loss(prediction_fake, fake_data_target(fake_data.size(0)))
+    error_fake.backward()
+
+    optimizer.step()
+    return error_real + error_fake, prediction_real, prediction_fake
